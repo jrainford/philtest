bcc9ddf39311d5b0ee7ee4641df1ac2d52dd464a
John Rainford <jrainford@gmail.com>
2018-12-08 19:01:36 +0000

    OK - basic working click to change from black to white and back

--- Philtest/ViewController.swift
+++ Philtest/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+
+    @IBAction func philButton(_ sender: Any) {
+        lightOn = !lightOn
+        
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
 
